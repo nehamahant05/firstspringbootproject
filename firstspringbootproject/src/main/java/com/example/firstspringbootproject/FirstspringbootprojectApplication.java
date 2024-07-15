0efe3d3189d80b9f756ea4a85513ed1c0125cac8
Neha <neha.mahant05@gmail.com>
2024-07-15 11:55:03 -0700

    inial commit

---
+++ firstspringbootproject/src/main/java/com/example/firstspringbootproject/FirstspringbootprojectApplication.java
@@ -0,0 +1,16 @@
+package com.example.firstspringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstspringbootprojectApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(FirstspringbootprojectApplication.class, args);
+
+
+	}
+
+}
